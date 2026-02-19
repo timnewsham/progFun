9c4883a7f7fc203ca1b389b30b153a34fc81e6f7
Tim Newsham <tim@fly.io>
2026-02-19 07:23:33 -1000

    make code easier to understand for math people, and support random seeds

--- aut.py
+++ aut.py
@@ -3,17 +3,24 @@ import sys
 """
 Cellular automata.
 """
+import random
 
 def bits(n, sz):
     """Convert a number into a bit string of size sz."""
-    return [(n >> sh) & 1 for sh in range(sz)]
+    #return [(n >> sh) & 1 for sh in range(sz)]
+    return [(n // 2**p) % 2 for p in range(sz)]
 
 def unbits(bs):
-    return sum(b << sh for sh,b in enumerate(bs))
+    #return sum(b << sh for sh,b in enumerate(bs))
+    return sum(bs[n] * (2**n) for n in range(len(bs)))
 
 def seed(sz):
     """Return a vector of length sz whose elements are all zero except the middle one."""
     return [1 if n == (sz//2) else 0 for n in range(sz)]
+
+def rseed(sz):
+    """Return a vector of length sz whose elements are chosen from random."""
+    return [random.choice([0,1]) for n in range(sz)]
 
 def next_val(rulev, neigh):
     """Return the next value given neighbors [a,b,c] using rulevector"""
@@ -42,7 +49,7 @@ def show_rule(rulev):
         print(f"[{show(inp)}] -> [{show(outp)}]")
     print()
 
-def run(rule, sz):
+def run(rule, sz, seedfunc=seed):
     """Run the evolution of a seed using the rule."""
     # each rule number from zero to 256 makes a rulevector of size 8.
     assert 0 <= rule and rule < 256
@@ -51,7 +58,7 @@ def run(rule, sz):
 
     # start with a single seed value and 
     # run forever while evolving using the rule vector
-    v = seed(sz)
+    v = seedfunc(sz)
     while True:
         print(show(v))
         v = evolve(rulev, v)
@@ -60,13 +67,16 @@ def run(rule, sz):
 def main():
     size = 75 
     rule = 90
+    s = seed
     if len(sys.argv) > 1:
         rule = int(sys.argv[1])
     if len(sys.argv) > 2:
         size = int(sys.argv[2])
+    if len(sys.argv) > 3 and sys.argv[3][0] == 'r':
+        s = rseed
     print(f"rule {rule}, size {size}")
 
-    run(rule, size)
+    run(rule, size, s)
 
 if __name__ == '__main__':
     main()
